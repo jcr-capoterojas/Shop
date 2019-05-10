a21922b4b727c51099415f30e92870a298247962
jlcapoterojas <capotejl.fmcr12345@gmail.com>
2019-05-09 21:22:02 -0500

    Add products controller

---
+++ Shop.Web/Date/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Shop.Web.Date
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Date.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)//Para que se conete a la base de datso
+        {
+
+        }
+    }
+}
